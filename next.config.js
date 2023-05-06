7cc00928ad8608a695e5df29d74aa3647eef77fb
Junaed254 <127419593+Junaed254@users.noreply.github.com>
2023-05-06 11:03:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
 
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: "public"
+  },
   reactStrictMode: false,
   images: {
-    domains: ["gogocdn.net","cdn.myanimelist.net"],
-  },
-};
+    domains: ["gogocdn.net", "cdn.myanimelist.net"]
+  }
+});
